1be08ab09f949be8f1586453752d41d41831f530
Eduardo Melo <nascimentomeloedu@gmail.com>
2022-06-13 20:34:47 -0300

    api route fixed

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
-const Api = axios.create({ baseURL: 'http://localhost:8080' })
+require('dotenv').config();
+const Api = axios.create({ baseURL: process.env.REACT_APP_BASE_API });
 
 export { Api }
