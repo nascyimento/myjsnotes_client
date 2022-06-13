dcd48ef3bff3e71385b95061a896f75f4a5e2178
Eduardo Melo <nascimentomeloedu@gmail.com>
2022-06-13 15:20:09 -0300

    path variable corrected

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
 import axios from "axios";
-const Api = axios.create({ baseURL: process.env.REAC_APP_BASE_API });
+const Api = axios.create({ baseURL: process.env.REACT_APP_BASE_API });
 
 export { Api }
